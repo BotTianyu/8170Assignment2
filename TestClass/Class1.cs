b8f9bd7fccc3080212dac5f28af192b1460dafe2
Tianyu Dai <Tdai6259@conestogac.on.ca>
2023-06-07 22:16:27 -0400

    Solution added

---
+++ TestClass/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit.Framework;
+
+
+namespace TestClass
+{
+    public class Class1
+    {
+        [TestFixture]
+
+            public class TriangleTest1
+        {
+            [Test]
+
+            public void ValidTriangle_Input60and60and60_OutputValidTriangle()
+            {
+
+                int firstSide = 60;
+                int secondSide = 60;
+                int thirdSide = 60;
+
+                string expected = "The triangle is valid and is an EQUILATERAL";
+
+
+                string actual = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
